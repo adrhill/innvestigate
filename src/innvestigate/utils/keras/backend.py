e8590c9ac0e51e7ed0770f2d8c99b6cbe517746d
Adrian Hill <adrian.hill@mailbox.org>
2021-03-30 18:55:11 +0200

    Remove Theano backend and CNTK stubs

--- src/innvestigate/utils/keras/backend.py
+++ src/innvestigate/utils/keras/backend.py
@@ -31,49 +31,13 @@ def gradients(Xs, Ys, known_Ys):
     :param known_Ys: Gradients for Ys.
     :return: Gradients for Xs given known_Ys
     """
-    backend = K.backend()
-    if backend == "theano":
-        # no global import => do not break if module is not present
-        assert len(Ys) == 1
-        import theano.gradient
-
-        known_Ys = {k: v for k, v in zip(Ys, known_Ys)}
-        # TODO: check the stop gradient issue here!
-        return theano.gradient.grad(K.sum(Ys[0]), Xs, known_grads=known_Ys)
-    elif backend == "tensorflow":
-        # no global import => do not break if module is not present
-        import tensorflow
-
-        return tensorflow.gradients(Ys, Xs, grad_ys=known_Ys, stop_gradients=Xs)
-    else:
-        # TODO: add cntk
-        raise NotImplementedError()
-
-
-###############################################################################
+    return tensorflow.gradients(Ys, Xs, grad_ys=known_Ys, stop_gradients=Xs)
 
 
 def is_not_finite(x):
     """Checks if tensor x is finite, if not throws an exception."""
-
-    backend = K.backend()
-    if backend == "theano":
-        # no global import => do not break if module is not present
-        import theano.tensor
-
-        return theano.tensor.or_(theano.tensor.isnan(x), theano.tensor.isinf(x))
-    elif backend == "tensorflow":
-        # no global import => do not break if module is not present
-        import tensorflow
-
-        # x = tensorflow.check_numerics(x, "innvestigate - is_finite check")
-        return tensorflow.logical_not(tensorflow.is_finite(x))
-    else:
-        # TODO: add cntk
-        raise NotImplementedError()
-
-
-###############################################################################
+    # x = tensorflow.check_numerics(x, "innvestigate - is_finite check")
+    return tensorflow.logical_not(tensorflow.is_finite(x))
 
 
 def extract_conv2d_patches(x, kernel_shape, strides, rates, padding):
@@ -86,63 +50,26 @@ def extract_conv2d_patches(x, kernel_shape, strides, rates, padding):
     :param padding: Paddings of the Keras conv2d layer.
     :return: The extracted patches.
     """
+    if K.image_data_format() == "channels_first":
+        x = K.permute_dimensions(x, (0, 2, 3, 1))
+    kernel_shape = [1, kernel_shape[0], kernel_shape[1], 1]
+    strides = [1, strides[0], strides[1], 1]
+    rates = [1, rates[0], rates[1], 1]
+    ret = tensorflow.extract_image_patches(
+        x, kernel_shape, strides, rates, padding.upper()
+    )
 
-    backend = K.backend()
-    if backend == "theano":
-        # todo: add theano function.
-        raise NotImplementedError()
-    elif backend == "tensorflow":
-        # no global import => do not break if module is not present
-        import tensorflow
-
-        if K.image_data_format() == "channels_first":
-            x = K.permute_dimensions(x, (0, 2, 3, 1))
-        kernel_shape = [1, kernel_shape[0], kernel_shape[1], 1]
-        strides = [1, strides[0], strides[1], 1]
-        rates = [1, rates[0], rates[1], 1]
-        ret = tensorflow.extract_image_patches(
-            x, kernel_shape, strides, rates, padding.upper()
-        )
-
-        if K.image_data_format() == "channels_first":
-            # todo: check if we need to permute again.xs
-            pass
-        return ret
-    else:
-        # TODO: add cntk
-        raise NotImplementedError()
-
-
-###############################################################################
+    if K.image_data_format() == "channels_first":
+        # TODO: check if we need to permute again.xs
+        pass
+    return ret
 
 
 def gather(x, axis, indices):
-    """Works as TensorFlow's gather."""
-    backend = K.backend()
-    if backend == "theano":
-        # TODO: add theano function.
-        raise NotImplementedError()
-    elif backend == "tensorflow":
-        # no global import => do not break if module is not present
-        import tensorflow
-
-        return tensorflow.gather(x, indices, axis=axis)
-    else:
-        # TODO: add cntk
-        raise NotImplementedError()
+    """TensorFlow's gather."""
+    return tensorflow.gather(x, indices, axis=axis)
 
 
 def gather_nd(x, indices):
-    """Works as TensorFlow's gather_nd."""
-    backend = K.backend()
-    if backend == "theano":
-        # TODO: add theano function.
-        raise NotImplementedError()
-    elif backend == "tensorflow":
-        # no global import => do not break if module is not present
-        import tensorflow
-
-        return tensorflow.gather_nd(x, indices)
-    else:
-        # TODO: add cntk
-        raise NotImplementedError()
+    """TensorFlow's gather_nd."""
+    return tensorflow.gather_nd(x, indices)
